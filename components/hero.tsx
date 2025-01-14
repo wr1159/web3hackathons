a3eb441a0889d743817fd3bff012efe6f61bf158
wr1159 <chuweiron18@gmail.com>
2025-01-14 22:11:04 +0000

    Add get started page

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 export default function Header() {
     return (
         <div className="flex flex-col gap-16 items-center">
-            <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+            <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center font-bold">
                 Your One Stop for Web3 Hackathons!
             </p>
+            <Button>
+                <Link href="/hackathons">Start now!</Link>
+            </Button>
             <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
         </div>
     );
